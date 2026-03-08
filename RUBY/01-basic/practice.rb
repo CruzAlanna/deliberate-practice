f6815adbf0ecba1215021930a684f4392efbfc5a
CruzAlanna <lannikcru@gmail.com>
2026-03-08 16:11:42 +1000

    Modified files for JSand PY to correct file type and exercise format, adding in practice file for next cohorts demo videos

---
+++ RUBY/01-basic/practice.rb
@@ -0,0 +1,10 @@
+# Use a variable to store a number, then write a condition that prints "Perfect" if the number is wqual to 25, and prints "Not quite" otherwise.
+
+number = 25
+
+if number == 25
+  puts "Perfect"
+else
+  puts "Not quite"
+end
+
